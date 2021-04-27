a179984ca74fc312f93b439d49075bb7f145da16
YaelDonat <71334737+YaelDonat@users.noreply.github.com>
2021-04-27 17:13:13 +0200

    praticien affichage recup données
    Recuperation des données de praticien + afficchage

--- src/Controller/PraticienController.php
+++ src/Controller/PraticienController.php
@@ -2,19 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\Praticien;
+use App\Repository\PraticienRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PraticienController extends AbstractController
 {
+    /** 
+     * @var PraticienRepository
+     */
+    private $repository;
+    public function __construct(PraticienRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
     /**
      * @Route("/praticien", name="praticien")
      */
     public function index(): Response
     {
+        $praticien = $this->repository->findAll();
         return $this->render('praticien/index.html.twig', [
-            'controller_name' => 'PraticienController',
+            'praticiens' => $praticien,
         ]);
     }
 }
