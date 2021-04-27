c45d55d670ef5ecc676424045324acc16c435dcb
YaelDonat <71334737+YaelDonat@users.noreply.github.com>
2021-04-27 16:54:10 +0200

    Update AppFixtures.php
    Données test au hasard de type praticien et praticien

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,12 +2,17 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Delegue;
+
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 use Faker\Factory;
+use App\Entity\Praticien;
+use App\Entity\RapportVisite;
+use App\Entity\TypePraticien;
+use App\Entity\Visiteur;
+use App\Entity\Delegue;
 
 class AppFixtures extends Fixture
 {
@@ -25,23 +30,34 @@ class AppFixtures extends Fixture
         $delegue->setPassword($this->encoder->encodePassword($delegue, '0000'));
         $delegue->setLogin('Delmt');
         $manager->persist($delegue);
+
+        $faker = Factory::create('fr_FR');
+        // on crée 4 auteurs avec noms et prénoms "aléatoires" en français
+
+        $typepraticien = array();
+
+        for ($i = 0; $i < 100; $i++) {
+            $typepraticien[$i] = new TypePraticien();
+            $typepraticien[$i]->setLibelle($faker->word);
+            $typepraticien[$i]->setLieu($faker->city);
+
+            $manager->persist($typepraticien[$i]);
+        }
+
+        $praticien = array();
+        for ($i = 0; $i < 100; $i++) {
+            $praticien[$i] = new Praticien();
+            $praticien[$i]->setType($typepraticien[$i % 3]);
+            $praticien[$i]->setNom($faker->lastName);
+            $praticien[$i]->setPrenom($faker->firstName);
+            $praticien[$i]->setAdresse($faker->address);
+            $praticien[$i]->setCp($faker->postcode);
+            $praticien[$i]->setVille($faker->city);
+            $praticien[$i]->setCoefNotoriete($faker->numberBetween($min = 1, $max = 3));
+
+            $manager->persist($praticien[$i]);
+        }
+
         $manager->flush();
     }
-
-
-
-}
-
-
-    abstract class BaseFixture extends Fixture
-{
-/** @var Generator */
-    protected $faker;
-
-    public function load(ObjectManager $manager)
-    {
-        $this->manager = $manager;
-        $this->faker = Factory::create();
-    }
-
 }
